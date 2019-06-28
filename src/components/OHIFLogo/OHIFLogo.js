12fa9244765b35f85902391ec1f08beac3967707
bugprone <bugprone@gmail.com>
2019-06-28 14:17:23 +0900

    logo change

--- src/components/OHIFLogo/OHIFLogo.js
+++ src/components/OHIFLogo/OHIFLogo.js
@@ -1,7 +1,9 @@
 import './OHIFLogo.css';
 
-import { Icon } from 'react-viewerbase';
+// import { Icon } from 'react-viewerbase';
 import React from 'react';
+
+import doai_logo from './doai-logo.png';
 
 function OHIFLogo() {
   return (
@@ -9,10 +11,15 @@ function OHIFLogo() {
       target="_blank"
       rel="noopener noreferrer"
       className="header-brand"
-      href="http://ohif.org"
+      href="http://wwww.doai.ai/"
     >
+      {/*
       <Icon name="ohif-logo" className="header-logo-image" />
       <div className="header-logo-text">Open Health Imaging Foundation</div>
+      */}
+
+      <img name="doai-logo" className="header-logo-image" src={logo} />
+      <div className="header-logo-text">DoAI Medical Platform</div>
     </a>
   );
 }
